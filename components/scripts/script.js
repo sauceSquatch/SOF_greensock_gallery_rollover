053f0e67c357147feaf8d0ef109da7bdf7eccd2c
Jamie Naioti <Jamie.Naioti@razorfish.com>
2015-07-20 16:57:43 -0500

    minor style update

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -23,7 +23,7 @@ onOver = function(evt) {
     var headlines = $(".headlines", this)
       subHead = $("h3", this)
       gradient_fade = $(".gradient_fade", this);
-    TweenLite.to($('h2', headlines), 0.25, {top:"-29", ease:Quint.easeOut});
+    TweenLite.to($('h2', headlines), 0.25, {top:"-29", ease:Quint.easeOut, opacity:1});
     TweenLite.to(this, 0.35, {backgroundSize:"155% 155%", ease:Quint.easeOut});
     TweenLite.to(gradient_fade, 0.35, {opacity:1});
     TweenLite.to(subHead, 0.15, {color:"#FFF"});
@@ -38,7 +38,7 @@ onOut = function(evt) {
     var headlines = $(".headlines", this)
       subHead = $("h3", this)
       gradient_fade = $(".gradient_fade", this);
-    TweenLite.to($('h2', headlines), 0.15, {top:"0", ease:Power2.easeOut});
+    TweenLite.to($('h2', headlines), 0.15, {top:"0", ease:Power2.easeOut, opacity:0.85});
     TweenLite.to(this, 0.35, {backgroundSize:"100% 100%", ease:Quint.easeOut});
     TweenLite.to(gradient_fade, 0.35, {opacity:0.5});
     TweenLite.to(subHead, 0.35, {color:"#333"});
