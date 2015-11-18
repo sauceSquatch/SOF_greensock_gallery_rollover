e97988c57ed7db247d7b0d08b2bae6559159de32
Jamie Naioti <naioti@gmail.com>
2015-11-18 16:37:00 -0600

    added gitignore, modified js to create object for hover block so it's more re-usable

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,47 +1,60 @@
 
 $(document).ready(function()
 {
-  initBuild();
+
+  TweenLite.to(hover_block, 0, {backgroundSize:"100% 100%"});
+
+  // initBuild();
 })
 
 ///// INIT
-var hover_block = $('.hover_block');
+var hover_block = new HoverBlock($('.hover_block')),
+totalItems = [];
 
-TweenLite.to(hover_block, 0, {backgroundSize:"100% 100%"});
+
 
 ///// INIT build
-initBuild = function() {
-  hover_block.on('mouseover', onOver);
-  hover_block.on('mouseout', onOut);
-}
+// initBuild = function() {
+//   hover_block.on('mouseover', onOver);
+//   hover_block.on('mouseout', onOut);
+// }
 
-onOver = function(evt) {
-  if( $(this).is(".clicked" && $(this).is(".hover")) ) {
-    // do nothing yet
-  } else {
-    $(this).addClass("hover");
-    var headlines = $(".headlines", this)
-      subHead = $("h3", this)
-      gradient_fade = $(".gradient_fade", this);
-    TweenLite.to($('h2', headlines), 0.25, {top:"-29", ease:Quint.easeOut, opacity:1});
-    TweenLite.to(this, 0.35, {backgroundSize:"155% 155%", ease:Quint.easeOut});
-    TweenLite.to(gradient_fade, 0.35, {opacity:1});
-    TweenLite.to(subHead, 0.15, {color:"#FFF"});
+function HoverBlock(obj) {
+
+  console.log(totalItems, " totalItems")
+
+  onOver = function(evt) {
+    if( obj.is(".clicked" && $(this).is(".hover")) ) {
+      // do nothing yet
+    } else {
+      obj.addClass("hover");
+      var headlines = $(".headlines", this)
+        subHead = $("h3", this)
+        gradient_fade = $(".gradient_fade", this);
+      TweenLite.to($('h2', headlines), 0.25, {top:"-29", ease:Quint.easeOut, opacity:1});
+      TweenLite.to(this, 0.35, {backgroundSize:"155% 155%", ease:Quint.easeOut});
+      TweenLite.to(gradient_fade, 0.35, {opacity:1});
+      TweenLite.to(subHead, 0.15, {color:"#FFF"});
+    }
   }
-}
 
-onOut = function(evt) {
-  if( $(this).is(".clicked" && $(this).not(".hover")) ) {
-    // do nothing yet
-  } else {
-    $(this).removeClass("hover");
-    var headlines = $(".headlines", this)
-      subHead = $("h3", this)
-      gradient_fade = $(".gradient_fade", this);
-    TweenLite.to($('h2', headlines), 0.15, {top:"0", ease:Power2.easeOut, opacity:0.85});
-    TweenLite.to(this, 0.35, {backgroundSize:"100% 100%", ease:Quint.easeOut});
-    TweenLite.to(gradient_fade, 0.35, {opacity:0.5});
-    TweenLite.to(subHead, 0.35, {color:"#333"});
+  onOut = function(evt) {
+    if( obj.is(".clicked" && $(this).not(".hover")) ) {
+      // do nothing yet
+    } else {
+      obj.removeClass("hover");
+      var headlines = $(".headlines", this)
+        subHead = $("h3", this)
+        gradient_fade = $(".gradient_fade", this);
+      TweenLite.to($('h2', headlines), 0.15, {top:"0", ease:Power2.easeOut, opacity:0.85});
+      TweenLite.to(this, 0.35, {backgroundSize:"100% 100%", ease:Quint.easeOut});
+      TweenLite.to(gradient_fade, 0.35, {opacity:0.5});
+      TweenLite.to(subHead, 0.35, {color:"#333"});
 
+    }
   }
-}
+
+  obj.on('mouseover', onOver);
+  obj.on('mouseout', onOut);
+};
+
